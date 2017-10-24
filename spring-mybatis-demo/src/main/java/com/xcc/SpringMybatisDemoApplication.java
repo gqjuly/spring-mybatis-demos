42a36b06107b6a59679e147c493f66d01e125e3f
xcc <18720524690@163.com>
2017-10-24 10:38:17 +0800

    IDEA整合GitHub测试

---
+++ spring-mybatis-demo/src/main/java/com/xcc/SpringMybatisDemoApplication.java
@@ -0,0 +1,14 @@
+package com.xcc;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@MapperScan("com.xcc.mapper")//将项目中对应的mapper类的路径加进来就可以
+public class SpringMybatisDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMybatisDemoApplication.class, args);
+	}
+}
---
+++ spring-mybatis-demo/src/main/java/com/xcc/service/UserService.java
@@ -0,0 +1,12 @@
+package com.xcc.service;
+
+import com.xcc.model.User;
+
+import java.util.List;
+
+public interface UserService {
+
+    int addUser(User user);
+
+    //List<User> findAllUser(int pageNum, int pageSize);
+}
